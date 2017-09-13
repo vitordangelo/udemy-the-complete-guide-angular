060e690eb3df8281b99ff40b4b46e4ebcb823dde
vitor <metroid.p2p@gmail.com>
2017-09-13 20:39:08 -0300

    more code

--- src/app/recipes/recipe.module.ts
+++ src/app/recipes/recipe.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RecipeStartComponent]
+  declarations: [RecipeStartComponent, RecipeEditComponent]
 })
 export class RecipeModule { }
